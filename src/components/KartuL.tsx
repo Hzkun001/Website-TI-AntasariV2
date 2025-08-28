5db4d52d9b02e2247c6d041595e302a654bfd601
Hzkun001 <ratihaura61@gmail.com>
2025-08-28 15:54:39 +0800

    Up

---
+++ src/components/KartuL.tsx
@@ -0,0 +1,11 @@
+import { Bubble } from 'pixel-retroui';
+
+function KartuL() {
+  return (
+    <Bubble direction="left" className='size-1/3 md:size-3/12 md:translate-y-20 md:translate-x-18 p-2' borderColor='#ED9003'>
+      Kenalan yuk, nama aku Yongki Kids
+    </Bubble>
+  );
+}
+
+export default KartuL;
---
+++ src/components/KartuR.tsx
@@ -0,0 +1,11 @@
+import { Bubble } from 'pixel-retroui';
+
+function KartuR() {
+  return (
+    <Bubble direction="right" className="translate-y-6 translate-x-4 md:translate-y-24 md:translate-x-20" borderColor='#ED9003'>
+    Halo apa kabar?
+    </Bubble>
+  );
+}
+
+export default KartuR;
